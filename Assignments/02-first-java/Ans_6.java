68edfddc344a5183ae349f532ae45101e9ea8c0e
RohanSahana <64703711+RohanSahana@users.noreply.github.com>
2021-09-20 00:56:42 +0530

    Delete Ans_6.java

--- Assignments/02-first-java/Ans_6.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Ans_6 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        System.out.println("Rs " + a + " = " + 0.014 *a + " USD");
-    }
-}
